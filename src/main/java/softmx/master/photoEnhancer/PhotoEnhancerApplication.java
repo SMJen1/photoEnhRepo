9c73f9d73f8a257b987ccb678f5487800fd07f23
student <sm.van@outlook.com>
2021-05-12 19:14:16 +0000

    Softmax first push

---
+++ src/main/java/softmx/master/photoEnhancer/PhotoEnhancerApplication.java
@@ -0,0 +1,13 @@
+package softmx.master.photoEnhancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoEnhancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoEnhancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/softmx/master/photoEnhancer/PhotoEnhancerApplicationTests.java
@@ -0,0 +1,13 @@
+package softmx.master.photoEnhancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoEnhancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
